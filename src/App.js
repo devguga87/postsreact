6088151ac5ba927725709d08c80a8a886a635d8f
Gustavo Seabra <devguga87@gmail.com>
2021-09-22 08:41:40 -0300

    feature:components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import { Posts } from './components/Posts';
+
+import { loadPosts } from './utils/load-posts';
+
+class App extends Component {
+  state = {
+    posts:[]
+  };
+
+  async componentDidMount(){
+    await this.loadPosts();
+  };
+
+  loadPosts = async () => {
+    const postsAndPhotos = await loadPosts();
+    this.setState({posts:postsAndPhotos})
+  }
+
+  render(){
+    const { posts } = this.state;
+    return (
+      <section className="container">
+        <Posts posts={posts}/>
+      </section>
+    );
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
